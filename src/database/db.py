a3826c4f9b66bc45b144ab735287998349addd9d
vas g <vas.g@me.com>
2024-03-11 12:30:51 +0400

    edit deps

--- src/database/db.py
+++ src/database/db.py
@@ -1,5 +1,5 @@
-from src.config import settings
-from src.database.database_connector import DatabaseConnector
+from config import settings
+from database.database_connector import DatabaseConnector
 
 
 db = DatabaseConnector(url=settings.aiosqlite_db_url, echo=settings.db_echo)
