20c94e488bc072655287191d59bada2ae29b168d
vas g <vas.g@me.com>
2024-03-15 03:36:30 +0400

    some blink1 signals

--- src/internal/blink1.py
+++ src/internal/blink1.py
@@ -1,15 +1,32 @@
 import aiohttp
+from aiohttp import BasicAuth
 
 from config import settings
 
 
 async def blink1_red():
     async with aiohttp.ClientSession() as session:
+        auth = BasicAuth(settings.NGROK_USER.get_secret_value(), settings.NGROK_PASS.get_secret_value())
         ngrok_url = settings.NGROK_URL.get_secret_value()
-        await session.get(ngrok_url + 'blink')
+        await session.get(ngrok_url + 'blink', auth=auth)
 
 
 async def blink1_yellow():
     async with aiohttp.ClientSession() as session:
+        auth = BasicAuth(settings.NGROK_USER.get_secret_value(), settings.NGROK_PASS.get_secret_value())
         ngrok_url = settings.NGROK_URL.get_secret_value()
-        await session.get(ngrok_url + 'blink/yellow')
+        await session.get(ngrok_url + 'blink/yellow', auth=auth)
+
+
+async def blink1_magenta():
+    async with aiohttp.ClientSession() as session:
+        auth = BasicAuth(settings.NGROK_USER.get_secret_value(), settings.NGROK_PASS.get_secret_value())
+        ngrok_url = settings.NGROK_URL.get_secret_value()
+        await session.get(ngrok_url + 'blink/magenta', auth=auth)
+
+
+async def blink1_green():
+    async with aiohttp.ClientSession() as session:
+        auth = BasicAuth(settings.NGROK_USER.get_secret_value(), settings.NGROK_PASS.get_secret_value())
+        ngrok_url = settings.NGROK_URL.get_secret_value()
+        await session.get(ngrok_url + 'blink/magenta', auth=auth)
