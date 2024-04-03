aa64e9b96023ff72871b36eb8f521f83ccba02bc
vas g <vas.g@me.com>
2024-04-04 01:39:55 +0400

    notify admin setting

--- src/internal/notify_admin.py
+++ src/internal/notify_admin.py
@@ -1,11 +1,18 @@
-from aiogram import Bot
+import os
 
+from aiogram import Bot
 from config import settings
 
 
 async def on_startup_notify(bot: Bot):
-    await bot.send_message(settings.ADMINS[0], 'Bot started', disable_notification=True)
+    await bot.send_message(
+        settings.ADMINS[0],
+        f'{os.getcwd().split(os.sep)[-1].capitalize()} started\n\n/start',
+        disable_notification=True,
+    )
 
 
 async def on_shutdown_notify(bot: Bot):
-    await bot.send_message(settings.ADMINS[0], 'Bot shutdown', disable_notification=True)
+    await bot.send_message(
+        settings.ADMINS[0], f'{os.getcwd().split(os.sep)[-1].capitalize()} shutdown', disable_notification=True,
+    )
